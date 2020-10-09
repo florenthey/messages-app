9f0ed2c8a41b2646759456445bceb0770230c032
Mr-H <mr-h@mbp-de-mr-h.home>
2020-10-09 22:52:04 +0200

    fix(next.config.js): enabling cors

--- next.config.js
+++ next.config.js
@@ -1,7 +1,25 @@
 const { process } = require("babel-jest");
 
 module.exports = {
-  // env: {
-  //   DB: process.env.DB,
-  // },
+  async headers() {
+    return [
+      {
+        // mathching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
 };
